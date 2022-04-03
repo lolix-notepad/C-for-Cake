a3f959b81b32b4954b3a38f2c02bbdf95303aa64
none <none@none.com>
2022-04-03 12:41:19 -0500

    feature/improved styles and added more content

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # C-for-Cake
 
 ![alt text](https://aprogrammerlife.com/images/pictuers/lost_children_will_be_taught_the_c_programming_language.jpg)
+
+This is a website with the goal of teaching the programming language C as easy as possible, that's why it's named 'C for Cake'.
+
+The website covers from basic concepts and increases the complexity until avanced concepts with examples like building dicord bots or HTTP clients/servers.
+
+## Design
+
+The design is based in [nes.css](https://nostalgic-css.github.io/NES.css/)
---
+++ index.js
@@ -0,0 +1,80 @@
+// bar searcher
+const topicSearcher = document.getElementById('search-topic');
+const listTopicResults = document.getElementById('list-topic-results');
+
+const indexToLink = {
+    'installation': '/views/installation.html',
+    'introduction': '/views/intro.html',
+    'hello world program': '/views/hello-world.html',
+};
+
+const indexes = Object.keys(indexToLink);
+
+let term;
+
+const filterByProximatedTerm = (term, indexes) => {
+    let regex = RegExp(`.*${term.split('').join('.*')}.*`)
+
+    return indexes.filter(index => regex.test(index))
+}
+
+const renderResults = () => {
+    listTopicResults.style.display = 'block';
+
+    let indexesToRender = term ?
+        filterByProximatedTerm(term, indexes) :
+        indexes;
+
+    if (!indexesToRender.length)
+        return;
+
+    if (indexesToRender.length > 5)
+        indexesToRender = indexesToRender.slice(0, 5);
+
+    listTopicResults.innerHTML = '';
+
+    for (const index of indexesToRender) {
+        const p = document.createElement('p');
+        p.textContent = index;
+        p.onclick = () => {
+            window.location.replace(indexToLink[index])
+        };
+        listTopicResults.appendChild(p);
+    }
+}
+
+topicSearcher.onclick = renderResults;
+
+topicSearcher.oninput = (e) => {
+    term = e.target.value.toLowerCase();
+    renderResults();
+}
+
+// show/hide bar searcher (mobile view)
+const btnSearchTopic = document.getElementById('btn-search-topic');
+const barSeacherContainer = document.querySelector('.searcher');
+
+btnSearchTopic.onclick = () => {
+    barSeacherContainer.style.display = 'block';
+    topicSearcher.value = '';
+    topicSearcher.focus();
+    renderResults();
+}
+
+barSeacherContainer.onclick = () => {
+    barSeacherContainer.style.display = "none";
+    term = '';
+}
+
+// dynamic contents
+const contentTable = document.getElementById('content-table');
+
+if (contentTable) {
+    for (const index of indexes) {
+        const a = document.createElement('a');
+        a.textContent = index;
+        a.href = indexToLink[index];
+
+        contentTable.appendChild(a);
+    }
+}
